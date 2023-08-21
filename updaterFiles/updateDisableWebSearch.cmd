ef68c543d0d8630cfb97f33f830e0c84e5ffd451
Lucas Gabriel (lucmsilva) <lucmsilva651@gmail.com>
2023-08-21 17:02:06 -0300

    Update updateDisableWebSearch.cmd

--- updaterFiles/updateDisableWebSearch.cmd
+++ updaterFiles/updateDisableWebSearch.cmd
@@ -1,5 +1,5 @@
 @echo off
 echo Downloading: Latest ClassicAltTab release from "github.com"
 cd updaterCore
-updaterCoreMain https://github.com/kruct/classicalttab/archive/refs/heads/main.zip
+updaterCoreMain https://github.com/kruct/diswebsearch/archive/refs/heads/main.zip
 exit
